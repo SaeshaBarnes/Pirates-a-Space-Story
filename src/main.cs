3252d163a532259535982082be892bd0a1e748f9
SaeshaBarnes <savbarnes11@gmail.com>
2022-03-06 17:43:25 -0800

    First comit

---
+++ src/main.cs
@@ -0,0 +1,14 @@
+//first lines
+using System;
+using System.Colllections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace testingProject {
+    class Program {
+        static void Main() {
+            Console.Write("Hello World");
+            Console.ReadLine();
+        }
+    }
+}
